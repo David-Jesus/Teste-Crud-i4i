8fc63efbe9bc5fea424bc78c5df4646ee8bf3311
David-Jesus <67175850+David-Jesus@users.noreply.github.com>
2021-10-02 16:42:48 -0300

    commit

--- front-end/frontend/src/pages/cadastroPessoa/input_telefone.js
+++ front-end/frontend/src/pages/cadastroPessoa/input_telefone.js
@@ -3,7 +3,7 @@ import React from "react";
 import InputMask from "react-input-mask";
 
 const Input = ({value, onChange}) => (
-  <InputMask mask="(99)9 9999-9999" value={value} onChange={onChange} >{() => <TextField  label="Cpf" defaultValue="cpf" variant="outlined"/>}</InputMask>
+  <InputMask mask="(99) 9 9999-9999" value={value} onChange={onChange} >{() => <TextField  label="Telefone" defaultValue="cpf" variant="outlined"/>}</InputMask>
 );
 
 export default Input;
--- front-end/frontend/src/pages/listaPessoas/listaPessoa.js
+++ front-end/frontend/src/pages/listaPessoas/listaPessoa.js
@@ -7,8 +7,9 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import api from '../pag';
+import api from '../service/api';
 import Button from '@material-ui/core/Button';
+import { Route, useHistory, Switch, Redirect } from "react-router-dom";
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -34,68 +35,80 @@ const useStyles = makeStyles({
   },
 });
 
-export default function ListaUsuarios() {
+export default function ListaPessoas() {
   const classes = useStyles();
-  const [usuarios, setUsuarios] = useState([]);
+  const [pessoas, setPessoas] = useState([]);
+  const [redirect, setState] = useState(false);
+  const token = window.sessionStorage.getItem('token');
+  const config = {
+    headers: {
+      'x-access-token': token
+    }
+  }
 
-  async function handleDeleteUsuarios(idusuario) {
-     
+
+  async function handleDeletePessoa(idpessoa) {
+     alert(idpessoa)
     try {
-      await api.delete(`usuario/${idusuario}`, {});
-      setUsuarios(usuarios.filter(usuario => usuario.idusuario !== idusuario));
-    } catch (error) {
-      alert('Erro ao deletar cliente');
+      const del = await api.delete(`pessoa/${idpessoa}`, config);
+
+      if((await del).status === 200){
+        alert("Cadastro excluído com sucesso!")
+         setPessoas(pessoas.filter(pessoa => pessoa.id !== idpessoa));
+      }
+      if((await del).status === 401){
+        alert("Necessário realizar o login novamente!")
+        setState({ redirect: true });
+      }
+      else {
+        alert("Erro ao excluir cadastro!");
+      }
+     
+    } catch (AxiosError) {
+      if(AxiosError.response.status === 401){
+        alert("sua sessão expirou, necessário realizar o login novamente!")
+        setState({ redirect: true });
+      }
     }
   }
 
   useEffect(() => {
-    api.get('usuarios', {}).then(response => {
-      setUsuarios(response.data);
+    api.get('pessoas', config).then(response => {
+      setPessoas(response.data);
     });
   }, []);
+  if (redirect) {
+    return <Redirect to='login' />;
+  }
 
   return (
-    <div id="lista_cliente">
-      <div id="header_listacliente_link">
-        <div id="link_cliente_lista">
-          <a href="home">HOME</a>
-        </div>
-        <div id="link_cliente_lista">
-          <a href="cadastrar-pet">CADASTRAR PET</a>
-        </div>
-        <div id="link_cliente_lista">
-          <a href="cadastrar-cliente">Cadastrar Cliente</a>
-        </div>
-        <div id="link_cliente_lista">
-          <a href="login">LOGIN</a>
-        </div>
-        <div id="link_cliente_lista">
-          <a href="faq">FAQ</a>
-        </div>
-      </div>
-
+    <div id="lista_pessoa">
       <TableContainer component={Paper} id="tabela">
         <Table className={classes.table} size="small" aria-label="a dense table">
           <TableHead>
             <TableRow>
-              <StyledTableCell >Código Usuário</StyledTableCell >
-              <StyledTableCell align="right">Códido do Cliente</StyledTableCell >
-              <StyledTableCell align="right">EMAIL</StyledTableCell >
-              <StyledTableCell align="right">Data de Criação</StyledTableCell >
-              <StyledTableCell align="center">Excluir</StyledTableCell >
+              <StyledTableCell align="center">Código Pessoa</StyledTableCell >
+              <StyledTableCell align="center">Nome</StyledTableCell >
+              <StyledTableCell align="center">CPF</StyledTableCell >
+              <StyledTableCell align="center">Telefone</StyledTableCell >
+              <StyledTableCell align="center">Cargo</StyledTableCell >
+              <StyledTableCell align="center">Idade</StyledTableCell >
+              <StyledTableCell align="center"></StyledTableCell >
             </TableRow>
           </TableHead>
           <TableBody>
-            {usuarios.map(usuario => (
-              <StyledTableRow key={usuario.idusuario}>
-                <StyledTableCell component="th" scope="row">
-                  {usuario.idusuario}
+            {pessoas.map(pessoa => (
+              <StyledTableRow key={pessoa.id}>
+                <StyledTableCell component="th" scope="row" align="center">
+                  {pessoa.id}
                 </StyledTableCell>
-                <StyledTableCell align="right">{usuario.idcliente}</StyledTableCell >
-                <StyledTableCell align="right">{usuario.email}</StyledTableCell >
-                <StyledTableCell align="right">{usuario.data_criacao}</StyledTableCell >
+                <StyledTableCell align="center">{pessoa.nome}</StyledTableCell >
+                <StyledTableCell align="center">{pessoa.cpf}</StyledTableCell >
+                <StyledTableCell align="center">{pessoa.telefone}</StyledTableCell >
+                <StyledTableCell align="center">{pessoa.cargo}</StyledTableCell >
+                <StyledTableCell align="center">{pessoa.idade}</StyledTableCell >
                 <StyledTableCell align="center">
-                  <Button variant="contained" color="secondary" type="button" onClick={() => handleDeleteUsuarios(usuario.idusuario)}>Deletar</Button></StyledTableCell >
+                  <Button variant="contained" color="secondary" type="button" onClick={() => handleDeletePessoa(pessoa.id)}>Deletar</Button></StyledTableCell >
               </StyledTableRow >
             ))}
           </TableBody>
