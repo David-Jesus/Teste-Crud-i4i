d4e5d34f594530886106c9e60141466e3ce5f522
David-Jesus <67175850+David-Jesus@users.noreply.github.com>
2021-10-02 16:43:37 -0300

    commit

--- front-end/frontend/src/routes.js
+++ front-end/frontend/src/routes.js
@@ -3,17 +3,21 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Login from './pages/login/login';
 import ListaUsuarios from './pages/listaUsuarios/listaUsuarios';
+import ListaPessoas from './pages/listaPessoas/listaPessoa';
+// import ListaPessoas from './pages/listaPessoas/listaPessoa';
 import CadastroUsuario from './pages/cadastroUsuario/cadastroUsuario';
 import CadastroPessoa from './pages/cadastroPessoa/cadastroPessoa';
+// import PersistentDrawerLeft from '../src/components/sidebar/Drawer';
 
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-               {/* <Route path='/upload' exact={true} component={Uploads} /> */}
                <Route path='/lista-usuarios' exact={true} component={ListaUsuarios} />
+               <Route path='/lista-pessoas' exact={true} component={ListaPessoas} />
                <Route path='/cadastro-usuario' exact={true} component={CadastroUsuario} />
                <Route path='/cadastro-pessoa' exact={true} component={CadastroPessoa} />
+               {/* <Route path='/navbar' exact={true} component={PersistentDrawerLeft} /> */}
                <Route path='/login' exact={true} component={Login} />
             </Switch>
         </BrowserRouter>
